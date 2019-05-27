b6ae000ec68aeba6cda5f2d4f6e324a607645e87
Kevin <kevinwmitchell.km@gmail.com>
2019-05-27 16:51:55 -0400

    Added Constructor to Car
    Issue creating list of Cars - had to create constructor in Car

--- 200339879_LessonThree_ClassProject/Controllers/CarController.cs
+++ 200339879_LessonThree_ClassProject/Controllers/CarController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _200339879_LessonThree_ClassProject.Models;
 
 namespace _200339879_LessonThree_ClassProject.Controllers
 {
     public class CarController : Controller
     {
+        List<Car> carList = new List<Car>() {
+            new Car (1, "Toyota", "Corolla", "Compact", 2017),
+            new Car (2, "Ford", "F-150", "Truck", 2018),
+            new Car (3, "Dodge", "Caravan", "Minivan", 2019)
+        };
         // GET: Car
         public ActionResult Index()
         {
-            return View();
+            //return View();
+
+            //this will redirect to home page 
+            //return RedirectToAction("Index", "Home");
+
+            return Content("/Car - This is a list of cars from Content()");
         }
     }
 }
--- 200339879_LessonThree_ClassProject/Models/Car.cs
+++ 200339879_LessonThree_ClassProject/Models/Car.cs
@@ -11,6 +11,17 @@ namespace _200339879_LessonThree_ClassProject.Models
         public string Brand { get; set; }
         public string Model { get; set; }
         public string Type { get; set; }
-        public int Year { get; set; }
+        public int Year { get; set; } 
+
+        public Car(int Id, string Brand, string Model, string Type, int Year)
+        {
+            this.Id = Id;
+            this.Brand = Brand;
+            this.Model = Model;
+            this.Type = Type;
+            this.Year = Year;
+        }
+
+       
     }
 }
